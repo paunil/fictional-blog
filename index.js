8ada7e13f988a0106bfe0b4840adb9102dddb331
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-06-20 19:13:00 +0200

    implement like limitation

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // load  current likes from server
 $.get('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/likes',
-  function (data, textStatus, jqXHR) { 
+  function (data, textStatus, jqXHR) {
     $("#like-count").text(data);
-  });
+});
 
 // set up onclick for like button   
-$("#like-button").click(function(){
-  $.post('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/likes',
-    function (data, textStatus, jqXHR) { 
-      $("#like-count").text(data);
-    });
+$("#like-button").click(function () {
+
+  // only allow one like per browser 
+  if (localStorage.getItem("hasLiked") !== "true") {
+    
+    localStorage.setItem("hasLiked", "true");
+
+    $.post('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/likes',
+      function (data, textStatus, jqXHR) {
+        $("#like-count").text(data);
+      });
+  }
+
 });
