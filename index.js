7ca5589632a3efedac862d3ce3d1b4bea90a9526
Paulina Nilsson <67747928+Paulina97@users.noreply.github.com>
2021-06-22 13:05:27 +0200

    implement comment section

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // load  current likes from server
 $.get('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/likes',
-  function (data, textStatus, jqXHR) {
+  function (data) {
     $("#like-count").text(data);
 });
 
@@ -13,9 +13,27 @@ $("#like-button").click(function () {
     localStorage.setItem("hasLiked", "true");
 
     $.post('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/likes',
-      function (data, textStatus, jqXHR) {
+      function (data) {
         $("#like-count").text(data);
-      });
+    });
   }
 
+});
+
+// load current comments from server
+$.getJSON('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/comments',
+  function (data) {
+    data.forEach(element => {
+      $("#published-comments").append('<div class="container col-6 d-flex flex-column justify-content-center" style="background-color: white; padding: 2%; margin-bottom: 1%;"><h6 id="published-name">'+element.name+'</h6><p id="published-comment">'+element.comment+'</p></div>');
+    });
+});
+
+// publish new comment
+$( "#comment-form" ).submit(function( event ) {
+  $.post('https://api.paulina.tech/v1/blogposts/93f29652d1e011ebabcf396dbf62158f/comments', $("#comment-form").serialize(), 
+  function (data) {
+    $("#published-comments").append('<div class="container col-6 d-flex flex-column justify-content-center" style="background-color: white; padding: 2%; margin-bottom: 1%;"><h6 id="published-name">'+data.name+'</h6><p id="published-comment">'+data.comment+'</p></div>');
+  });
+  event.preventDefault();
+  $("#comment-form").trigger("reset");
 });
